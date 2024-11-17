04cce32492ff5c7abaf0bbd09f3fe392d6aaac74
achicao <achicao@unal.edu.co>
2024-11-17 11:57:41 -0500

    Intento 8

--- televisores/control.py
+++ televisores/control.py
@@ -3,32 +3,28 @@ class Control:
     _tv = None
 
     def turnOn(self):
-        _tv.turnOn()
+        self._tv.turnOn()
     
     def turnOff(self):
-        _tv.turnOff()
+        self._tv.turnOff()
 
     def canalUp(self):
-        _tv.canalUp()
+        self._tv.canalUp()
 
     def canalDown(self):
-        _tv.canalDown
+        self._tv.canalDown
 
     def volumenUp(self):
-        _tv.volumenUp()
+        self._tv.volumenUp()
 
     def volumenDown(self):
-        _tv.volumenDown()
+        self._tv.volumenDown()
 
     def setCanal(self, num):
-        _tv.setCanal(num)
+        self._tv.setCanal(num)
 
     def setVolumen(self, num):
-        _tv.setVolumen(num)
-
-    def enlazar(self,tv):
-        self._tv = tv
-        tv.setControl(self)
+        self._tv.setVolumen(num)
 
     def getTv(self):
         return self._tv
